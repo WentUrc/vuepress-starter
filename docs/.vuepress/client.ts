380e610cc88eaca8be38e94e1eba4032e9b2992a
冰苷晶 <2749332490@qq.com>
2024-12-25 00:04:04 +0800

    ✨ feat: add cyber button

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,10 +2,12 @@ import { defineClientConfig } from 'vuepress/client'
 import Swiper from './theme/lib/client/features/components/Swiper.vue'
 import './styles/custom.css'
 import VPImageToAscii from './components/VPImageToAscii.vue'
+import VPGame1 from './components/VPGame1.vue'
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component('Swiper', Swiper)
     app.component('VPImageToAscii', VPImageToAscii)
+    app.component('VPGame1', VPGame1)
   },
 })
