dd96b75f75623861f6d88dd7d28ffaf8b1d6e6db
冰苷晶 <2749332490@qq.com>
2025-01-01 20:15:47 +0800

    🐞 fix: some changes

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -4,11 +4,9 @@ import './styles/custom.css'
 import VPImageToAscii from './components/VPImageToAscii.vue'
 import VPGame1 from './components/VPGame1.vue'
 import VPGame2 from './components/VPGame2.vue'
-import VPBackToTop1 from './components/VPBackToTop1.vue'
 import VPEmbedHTML from './components/VPEmbed/VPEmbedHTML.vue'
 import VPEmbedHTML1 from './components/VPEmbed/VPEmbedHTML1.vue'
 import VPEmbedHTML2 from './components/VPEmbed/VPEmbedHTML2.vue'
-import VPMeowWaa from './components/VPEmbed/VPMeowWaa.vue'
 import VPMeowWbb from './components/VPEmbed/VPMeowWbb.vue'
 
 
@@ -20,9 +18,7 @@ export default defineClientConfig({
     app.component('VPGame2',VPGame2)
     app.component('VPEmbedHTML',VPEmbedHTML)
     app.component('VPEmbedHTML1',VPEmbedHTML1)
-    app.component('VPBackToTop',VPBackToTop1)
     app.component('VPEmbedHTML2',VPEmbedHTML2)
-    app.component('VPMeowWaa', VPMeowWaa)
     app.component('VPMeowWbb', VPMeowWbb)
   },
 })
