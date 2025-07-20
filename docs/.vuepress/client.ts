270914e1d7a9eb01b41bdd6d2354f55b8d689be8
IGCrystal <IGCrystal@wenturc.com>
2025-07-20 23:59:36 +0800

    ✨ feat: aside-nav

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,14 @@
 import { defineClientConfig } from 'vuepress/client'
+import { h } from 'vue'
 import './styles/custom.css'
 import Layout from './components/Layout.vue'
+import AsideNav from './components/AsideNav.vue'
 
 export default defineClientConfig({
   layouts: {
-    Layout,
+    Layout: h(Layout, null, {
+      'aside-outline-after': () => h(AsideNav),
+    }),
   },
   enhance({ app }) {
     // 在 VuePress 应用中注册全局组件
